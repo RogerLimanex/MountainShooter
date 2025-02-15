d1c34aee719ef2655a5bd1c4427420aae410b9aa
RogerLP <limanex_lp@hotmail.com>
2025-02-15 18:03:20 -0300

    update 1

---
+++ code/Game.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        self.window = None
+
+    def run(self):
+        print('Setup Start')
+        pygame.init()
+        window = pygame.display.set_mode(size=(600, 480))
+        print('Setup End')
+
+        print('Loop Start')
+        while True:
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print('Quiting...')
+                    pygame.quit()  # Close Window
+                    quit()  # end pygame
