bb762d2f0fb52069205e0fda5aeddb8a8b21fa60
RogerLP <limanex_lp@hotmail.com>
2025-03-12 15:45:54 -0300

    Imports move player1

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,11 +1,13 @@
+
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Player import Player
 
 
 class EntityFactory:
 
     @staticmethod
-    def get_entity(entity_name: str, position=(0,0)):
+    def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
@@ -13,4 +15,5 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
-
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2))
